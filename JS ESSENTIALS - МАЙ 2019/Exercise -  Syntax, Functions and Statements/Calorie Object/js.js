448592bcdf42dedd628d89c569ca5f40a14af7eb
Mixail <gambler4o0o0@gmail.com>
2020-04-19 19:49:18 +0300

    Learn JS

--- JS ESSENTIALS - МАЙ 2019/Exercise -  Syntax, Functions and Statements/Calorie Object/js.js
+++ JS ESSENTIALS - МАЙ 2019/Exercise -  Syntax, Functions and Statements/Calorie Object/js.js
@@ -2,4 +2,4 @@ function calorieObject(arg){
     let array = arg;
     console.log(`${array[0]} : ${array[1]}, ${array[2]} : ${array[3]}, ${array[4]} : ${array[5]}`);
 }
-calorieObject(['Potato',48,'Rise',138,'Apple',52])
+calorieObject(['Potato',48,'Rise',50,'Apple',20])
